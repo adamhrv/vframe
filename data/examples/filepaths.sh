648c22548281f81e695b05883918c6e3f1d80d87
Adam Harvey <adam@ahprojects.com>
2020-09-07 22:06:45 +0200

    add

--- data/examples/filepaths.sh
+++ data/examples/filepaths.sh
@@ -5,14 +5,14 @@
 DIR_EXAMPLES="../data/examples"
 DIR_IMAGES=${DIR_EXAMPLES}"/images"
 DIR_IMAGES_OUT=${DIR_IMAGES}"/output"
-DIR_IMAGES_3D=${DIR_IMAGES}"/3d"
 DIR_VIDEOS=${DIR_EXAMPLES}"/videos"
+DIR_VIDEO_OUT=${DIR_VIDEOS}"/output"
 
 # face images
-FP_SNOWDEN_X1=${DIR_IMAGES_3D}"/face-snowden-x1.png"
-FP_SNOWDEN_X3=${DIR_IMAGES_3D}"/face-snowden-x3.png"
+SNOWDEN_X1=${DIR_IMAGES}"/face-snowden-x1.png"
+SNOWDEN_X3=${DIR_IMAGES}"/face-snowden-x3.png"
 
 # face videos
-FP_SNOWDEN_X1_VIDEO=${DIR_IMAGES_3D}"/face-snowden-x1.mp4"
+SNOWDEN_X1_VIDEO=${DIR_VIDEOS}"/face-snowden-x1.mp4"
 
 echo "Added VFRAME example filepaths to environment"
