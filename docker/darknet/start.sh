3008a526aabeadd811e86b9ea3fc24d5958dccb2
adamhrv <adam@ahprojects.com>
2020-10-28 00:20:47 +0100

    migrate

---
+++ docker/darknet/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Startup script for docker image
+
+IMAGE=vframe/darknet
+#docker images $IMAGE
+xhost +local:docker
+DOCKER_NAME="$(echo $IMAGE | sed 's/\//-/g' | sed 's/:/-/g' | sed 's/_/-/g')"
+
+docker run \
+	-u $(whoami):$(whoami) \
+	-h $(hostname)-$DOCKER_NAME \
+	-it \
+	--gpus all \
+	-v /work:/work \
+	-v /data_store_ssd:/data_store_ssd \
+	-v /data_store_vframe:/data_store_vframe \
+	-w /work \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	$IMAGE /bin/zsh
